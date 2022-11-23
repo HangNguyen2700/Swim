167d214bde93f42b400550910d4efcc643aa62a3
nguyenhangnguyen <laniday2711@gmail.com>
2022-11-23 20:18:25 +0100

    add and update entity test and Kdoc

--- src/test/kotlin/entity/GameTest.kt
+++ src/test/kotlin/entity/GameTest.kt
@@ -4,26 +4,159 @@ import kotlin.test.*
 
 /**
  * Test cases for [Game]
+ *
+ * @constructor Create empty Game test
  */
 
 class GameTest {
-    private val c1 = Card(CardSuit.SPADES, CardValue.ACE)
-    private val c2 = Card(CardSuit.CLUBS, CardValue.JACK)
-    private val c3 = Card(CardSuit.HEARTS, CardValue.QUEEN)
-    private val c4 = Card(CardSuit.DIAMONDS, CardValue.EIGHT)
+    private val playerCards1 = mutableListOf<Card>(
+        Card(CardSuit.CLUBS, CardValue.QUEEN),
+        Card(CardSuit.SPADES, CardValue.TEN),
+        Card(CardSuit.DIAMONDS, CardValue.SEVEN),
+    )
+    private val player1 = Player("Bob", playerCards1)
 
-//    private val game = Game(0, listOf(c1,c2,c3), listOf(c4))
-//
-//    /**
-//     * Test if entered data in [Game]object is valid
-//     */
-//
-//    @Test
-//    fun testInitializedGame(){
-//        assertEquals(0, game.passCounter)
-//        assertEquals(listOf(c1,c2,c3), game.openCards)
-//        assertEquals(listOf(c4), game.unusedCards)
-//    }
+    private val playerCards2 = mutableListOf<Card>(
+        Card(CardSuit.CLUBS, CardValue.EIGHT),
+        Card(CardSuit.CLUBS, CardValue.NINE),
+        Card(CardSuit.HEARTS, CardValue.KING),
+    )
+    private val player2 = Player("Alice", playerCards2)
 
+    private val playerCards3 = mutableListOf<Card>(
+        Card(CardSuit.DIAMONDS, CardValue.QUEEN),
+        Card(CardSuit.SPADES, CardValue.QUEEN),
+        Card(CardSuit.DIAMONDS, CardValue.JACK),
+    )
+    private val player3 = Player("Ana", playerCards3)
 
+    private val players = mutableListOf<Player>(player1, player2, player3)
+
+    private val openCards = mutableListOf<Card>(
+        Card(CardSuit.SPADES, CardValue.SEVEN),
+        Card(CardSuit.DIAMONDS, CardValue.KING),
+        Card(CardSuit.DIAMONDS, CardValue.NINE),
+    )
+
+    private val unusedCards = mutableListOf<Card>(
+        Card(CardSuit.CLUBS, CardValue.JACK),
+        Card(CardSuit.HEARTS, CardValue.ACE),
+        Card(CardSuit.SPADES, CardValue.NINE),
+
+        Card(CardSuit.CLUBS, CardValue.ACE),
+        Card(CardSuit.SPADES, CardValue.JACK),
+        Card(CardSuit.HEARTS, CardValue.SEVEN),
+
+        Card(CardSuit.CLUBS, CardValue.SEVEN),
+        Card(CardSuit.CLUBS, CardValue.KING),
+        Card(CardSuit.DIAMONDS, CardValue.EIGHT),
+
+        Card(CardSuit.CLUBS, CardValue.TEN),
+        Card(CardSuit.DIAMONDS, CardValue.TEN),
+        Card(CardSuit.HEARTS, CardValue.JACK),
+
+        Card(CardSuit.SPADES, CardValue.KING),
+        Card(CardSuit.DIAMONDS, CardValue.ACE),
+        Card(CardSuit.HEARTS, CardValue.QUEEN),
+
+        Card(CardSuit.SPADES, CardValue.ACE),
+        Card(CardSuit.HEARTS, CardValue.NINE)
+    )
+
+    private val game = Game(players, openCards, unusedCards)
+
+    /**
+     * Test get players
+     *
+     */
+    @Test
+    fun testGetPlayers(){
+        assertEquals(mutableListOf(player1, player2, player3), game.getPlayers())
+    }
+
+    /**
+     * Test get current player
+     *
+     */
+    @Test
+    fun testGetCurrentPlayer(){
+        assertEquals(player1, game.getCurrentPlayer())
+    }
+
+    /**
+     * Test set current player
+     *
+     */
+    @Test
+    fun testSetCurrentPlayer(){
+        game.setCurrentPlayer(player3)
+        assertEquals(player3, game.getCurrentPlayer())
+    }
+
+    /**
+     * Test reset current player
+     *
+     */
+    @Test
+    fun testResetCurrentPlayer(){
+        game.resetCurrentPlayer()
+        assertEquals(player1, game.getCurrentPlayer())
+    }
+
+    /**
+     * Test next player
+     *
+     */
+    @Test
+    fun testNextPlayer(){
+        assertEquals(player2, game.nextPlayer())
+    }
+
+    /**
+     * Test get pass counter
+     *
+     */
+    @Test
+    fun testGetPassCounter(){
+        assertEquals(0, game.getPassCounter())
+    }
+
+    /**
+     * Test increase pass counter
+     *
+     */
+    @Test
+    fun testIncreasePassCounter(){
+        game.increasePassCounter()
+        game.increasePassCounter()
+        assertEquals(2, game.getPassCounter())
+    }
+
+    /**
+     * Test reset pass counter
+     *
+     */
+    @Test
+    fun testResetPassCounter(){
+        game.resetPassCounter()
+        assertEquals(0, game.getPassCounter())
+    }
+
+    /**
+     * Test get open cards
+     *
+     */
+    @Test
+    fun testGetOpenCards(){
+        assertEquals(openCards, game.getOpenCards())
+    }
+
+    /**
+     * Test get unuesdcards
+     *
+     */
+    @Test
+    fun testGetUnuesdcards(){
+        assertEquals(unusedCards, game.getUnusedCards())
+    }
 }
--- src/test/kotlin/entity/PlayerTest.kt
+++ src/test/kotlin/entity/PlayerTest.kt
@@ -5,23 +5,72 @@ import kotlin.test.assertEquals
 
 /**
  * Test cases for [Player]
+ *
+ * @constructor Create empty Player test
  */
 
 class PlayerTest {
-    private val playerName = ""
-    private val playerScore = 0.0
-    private val playerHasKnocked = false
+    private val cards = mutableListOf<Card>(
+        Card(CardSuit.CLUBS, CardValue.QUEEN),
+        Card(CardSuit.SPADES, CardValue.TEN),
+        Card(CardSuit.DIAMONDS, CardValue.SEVEN)
+    )
 
-//    private val player = Player(playerName, playerScore, playerHasKnocked )
-//
-//    /**
-//     * Test if entered data in [Player]object is valid
-//     */
-//
-//    @Test
-//    fun testInitializedPlayer(){
-//        assertEquals(playerName, player.name)
-//        assertEquals(playerScore, player.score)
-//        assertEquals(playerHasKnocked, player.hasKnocked)
-//    }
+    private val player = Player("Alice", cards)
+
+    /**
+     * Test get name
+     *
+     */
+    @Test
+    fun testGetName(){
+        assertEquals("Alice", player.getName())
+    }
+
+    /**
+     * Test get score
+     *
+     */
+    @Test
+    fun testGetScore(){
+        assertEquals(0.0, player.getScore())
+    }
+
+    /**
+     * Test set score
+     *
+     */
+    @Test
+    fun testSetScore(){
+        assertEquals(12.0, player.setScore(12.0))
+        assertEquals(12.0, player.getScore())
+    }
+
+    /**
+     * Test has knocked
+     *
+     */
+    @Test
+    fun testHasKnocked(){
+        assertEquals(false, player.getHasKnocked())
+    }
+
+    /**
+     * Test set has knocked
+     *
+     */
+    @Test
+    fun testSetHasKnocked(){
+        assertEquals(true, player.setHasKnocked())
+        assertEquals(true, player.getHasKnocked())
+    }
+
+    /**
+     * Test get player cards
+     *
+     */
+    @Test
+    fun testGetPlayerCards(){
+        assertEquals(cards, player.getPlayerCards())
+    }
 }
